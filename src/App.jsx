1762934e28b5967163b04f9fb29480ffb37a18ea
Khalid Ashraf <i.khalidashraf@gmail.com>
2026-07-29 19:47:17 +0530

    Created a seat booking component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import SeatBooking from "./components/Seat-Booking";
+
+function App() {
+  return (
+    <div>
+      <SeatBooking
+        layout={{ rows: 8, seatsPerRow: 12, aislePostion: 6 }}
+        seatTypes={{
+          standard: { name: "standard", price: 100, rows: [0, 1, 2] },
+          premium: { name: "premium", price: 200, rows: [3, 4, 5] },
+          business: { name: "business", price: 300, rows: [6, 7] },
+        }}
+        bookedSeats={["A1", "A2", "A3", "C6", "C7", "E10", "E11", "G12", "H12"]}
+      />
+    </div>
+  );
+}
+
+export default App;
